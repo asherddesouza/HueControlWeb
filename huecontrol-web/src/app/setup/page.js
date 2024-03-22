3c508842937148036f3717d78f6753756ee39b41
Asher De Souza <asher.desouza@clearscore.com>
2024-03-22 17:00:43 +0000

    adding very basic page

---
+++ huecontrol-web/src/app/setup/page.js
@@ -0,0 +1,20 @@
+'use client'
+
+export default function Setup() {
+    // const v3 = require('node-hue-api').v3;
+    // async function getBridge() {
+    //     const results = await v3.discovery.nupnpSearch();
+    
+    //     // Results will be an array of bridges that were found
+    //     console.log(JSON.stringify(results, null, 2));
+    //   }
+
+
+
+    return (
+        <main>
+            <h1>Setup Page</h1>
+            <button onClick={null}></button>
+        </main>
+    )
+}
