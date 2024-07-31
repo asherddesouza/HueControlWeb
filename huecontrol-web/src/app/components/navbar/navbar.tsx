e5b885a867b6c36281f0c301d7a813c6cee87816
Asher De Souza <asher.desouza@clearscore.com>
2024-07-31 16:57:01 +0100

    styled setup page

--- huecontrol-web/src/app/components/navbar/navbar.tsx
+++ huecontrol-web/src/app/components/navbar/navbar.tsx
@@ -3,11 +3,11 @@ import "./navbar.css"
 
 export default function Navbar() {
     return(
-        <div>
+        <div className="navbar">
             <ul>
                 <li><a href="setup" className={"active"}>Setup</a></li>
                 <li><a href="dashboard">Dashboard</a></li>
-                <li  className={"settings"}><a href="settings">Settings</a></li>
+                <li className={"settings"}><a href="settings">Settings</a></li>
             </ul>
         </div>
     )
--- huecontrol-web/src/app/setup/page.client.tsx
+++ huecontrol-web/src/app/setup/page.client.tsx
@@ -1,53 +1,63 @@
-'use client'
+"use client";
 
 import Navbar from "../components/navbar/navbar";
+import styles from "./page.module.css";
 
 interface Model {
-    number: string;
-    description: string;
-    name: string;
-    serial: string;
+  number: string;
+  description: string;
+  name: string;
+  serial: string;
 }
 
 interface Version {
-    major: string;
-    minor: string;
+  major: string;
+  minor: string;
 }
 
 interface Icon {
-    mimetype: string;
-    height: string;
-    width: string;
-    depth: string;
-    url: string;
+  mimetype: string;
+  height: string;
+  width: string;
+  depth: string;
+  url: string;
 }
 
 export interface BridgeInfo {
-    name: string;
-    manufacturer: string;
-    ipaddress: string;
-    model: Model;
-    version: Version;
-    icon: Icon[];
+  name: string;
+  manufacturer: string;
+  ipaddress: string;
+  model: Model;
+  version: Version;
+  icon: Icon[];
 }
 
 interface SetupProps {
-    bridgeInfo: BridgeInfo
+  bridgeInfo: BridgeInfo;
 }
 
-export default function Setup({bridgeInfo}: SetupProps) {
-    function getBridge() {
-        let parsedResults = (JSON.stringify(bridgeInfo, null, 2))
-        console.log(parsedResults)
-    }
-    return (
-        <main>
-            <Navbar/>
-            <h1>Welcome to HueControl!</h1>
-            <p>a simple app to control your Philips hue lightbulbs.</p>
-            <p>Made using TypeScript & Next.js</p>
-            <button onClick={getBridge}>Get Bridges</button>
-            <footer>© Asher De Souza 2024</footer>
-        </main>
-    )
+export default function Setup({ bridgeInfo }: SetupProps) {
+  function getBridge() {
+    let parsedResults = JSON.stringify(bridgeInfo, null, 2);
+    console.log(parsedResults);
+  }
+  return (
+    <main>
+      <Navbar />
+      <div className={styles.setup}>
+        <div className={styles.headerText}>
+          <h1 className={styles.title}>Welcome to HueControl!</h1>
+          <p className={styles.subtext}>
+            a simple app to control your Philips hue lightbulbs.
+          </p>
+        </div>
+
+        <div className={styles.background}>
+          <button onClick={getBridge}>Get Bridges</button>
+        </div>
+      </div>
+
+      <footer>© Asher De Souza 2024</footer>
+    </main>
+  );
 }
